827838eb5c1339f1d07375385674c6d9f16337fa
daniella-rly <daniellaamas@gmail.com>
2025-01-28 21:40:25 -0500

    Updated index.js with initial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
